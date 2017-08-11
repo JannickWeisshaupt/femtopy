08b56036eb1c83b914c47f879fdcf010db5b4f34
JannickWeisshaupt <jannickw@gmx.de>
2017-08-11 14:14:31 +0200

    Added animation of gaussian beam

--- GaussianOptics.py
+++ GaussianOptics.py
@@ -1,6 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def lens(f):
+    return np.array([[1, 0], [-1 / f, 1]])
+
+
+def dist(d):
+    return np.array([[1, d], [0, 1]])
 
 class GaussBeam:
     def __init__(self, lambda0, v1, v2=0, mode='w&R', Ep=1, fwhm_t=1):
@@ -48,20 +54,49 @@ class GaussBeam:
     def Imax0(self):
         return 2 * self.P / (np.pi * self.w0() ** 2)
 
+    @staticmethod
+    def _is_dist(m):
+        if m[0,0] == 1 and m[1,0]==0 and m[1,1] == 1 and m[0,1] != 0:
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def _is_lens(m):
+        if m[0, 0] == 1 and m[0, 1] == 0 and m[1, 1] == 1 and m[1, 0] != 0:
+            return True
+        else:
+            return False
+
+    def propagate_and_animate(self,prop_list,N=100,color='r',plot=True):
+        w_list = [self.w()]
+        d_vec = [0]
+        for m in prop_list:
+            if self._is_dist(m):
+                d_ges = m[0,1]
+                d_step = d_ges/N
+                for i in range(N):
+                    self.propagate(dist(d_step))
+                    d_vec.append(d_vec[-1]+d_step)
+                    w_list.append(self.w())
+            elif self._is_lens(m):
+                self.propagate(m)
+        if plot:
+            plt.plot(d_vec,np.array([w_list,-np.array(w_list)]).T,color=color)
+
+        return np.array(d_vec),np.array(w_list)
+
     def __repr__(self):
         return 'Actual Position: z = {0:2.3f} m.  w = {1:2.2e} m.    R = {2:1.2e} m.  Imax = {3:1.2e} W/cm^2\nFocus:           z0= {4:2.2f} m.  w0= {5:1.2e} m.  phi = {6:1.2e} rad.   Imax0 = {7:1.2e} W/cm^2  \n'.format(
             *[self.z(), self.w(), self.R(), self.Imax() * 1e-4, self.z0(), self.w0(), self.phi(), self.Imax0() * 1e-4])
 
 
-def lens(f):
-    return np.array([[1, 0], [-1 / f, 1]])
 
-
-def dist(d):
-    return np.array([[1, d], [0, 1]])
 
 
 if __name__ == '__main__':
     b1 = GaussBeam(400e-9, 0.05, 100000000)
     b2 = GaussBeam(266e-9, 0.05, 100000000)
-
+    l1 = lens(0.7)
+    d1 = dist(1)
+    res = b1.propagate_and_animate([d1,l1,d1],N=10000)
